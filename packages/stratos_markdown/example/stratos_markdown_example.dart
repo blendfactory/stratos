d8895439200f92eaf95b07c08242ff33a5b1ce3a
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-03-08 17:32:16 +0900

    feat: create stratos_markdown package

---
+++ packages/stratos_markdown/example/stratos_markdown_example.dart
@@ -0,0 +1,6 @@
+import 'package:stratos_markdown/stratos_markdown.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/stratos_markdown/test/stratos_markdown_test.dart
@@ -0,0 +1,16 @@
+import 'package:stratos_markdown/stratos_markdown.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
