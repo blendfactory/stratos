615d854a8981f6ea0f92494a42d95f9e12617596
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-03-08 17:30:39 +0900

    feat: create stratos_json package

---
+++ packages/stratos_json/example/stratos_json_example.dart
@@ -0,0 +1,6 @@
+import 'package:stratos_json/stratos_json.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/stratos_json/test/stratos_json_test.dart
@@ -0,0 +1,16 @@
+import 'package:stratos_json/stratos_json.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
